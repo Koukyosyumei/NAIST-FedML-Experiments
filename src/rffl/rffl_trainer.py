fix gradient update

src/rffl/rffl_trainer.py
@@ -14,23 +14,26 @@ from fedml_api.standalone.fedavg.my_model_trainer_classification import MyModelT
 
 
 class RFFL_ModelTrainer(MyModelTrainer):
-    def get_model_gradients(self, gamma=0.5):
-        grads = [param.grad for param in self.model.parameters()]
+    def get_model_gradients(self, gamma=0.5, weight=0.15):
+        grads = [weight * g for g in self.grads]
+        """
         flattened = flatten(grads)
         norm_value = norm(flattened) + 1e-7  # to prevent division by zero
         grads = unflatten(
             torch.multiply(torch.tensor(gamma), torch.div(flattened, norm_value)),
             grads,
         )
+        """
         return grads
 
-    def set_model_gradients(self, gradient, device, weight=1e3):
+    def set_model_gradients(self, gradient, device, weight=1):
         gradient = [grad.to(device) for grad in gradient]
 
         for param, grad in zip(self.model.parameters(), gradient):
             param.data += weight * grad.data
+        logging.info("update parameters !!!!!!!!!!!!!!!")
 
-    def train(self, train_data, device, args):
+    def train(self, train_data, device, local_sample_number, args):
         model = self.model
 
         model.to(device)
@@ -40,6 +43,9 @@ class RFFL_ModelTrainer(MyModelTrainer):
         criterion = nn.CrossEntropyLoss().to(device)
 
         epoch_loss = []
+        self.grads = [
+            torch.zeros(param.shape).to(device) for param in self.model.parameters()
+        ]
         for epoch in range(args.epochs):
             batch_loss = []
             for batch_idx, (x, labels) in enumerate(train_data):
@@ -50,7 +56,12 @@ class RFFL_ModelTrainer(MyModelTrainer):
                 loss.backward()
 
                 # to avoid nan loss
-                # torch.nn.utils.clip_grad_norm_(self.model.parameters(), 1.0)
+                torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+                temp_grad = [param.grad for param in model.parameters()]
+                for param_idx, tg in enumerate(temp_grad):
+                    self.grads[param_idx] += (x.shape[0] / local_sample_number) * tg
+
+                logging.info(self.grads[0])
 
                 # logging.info('Update Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 #     epoch, (batch_idx + 1) * args.batch_size, len(train_data) * args.batch_size,