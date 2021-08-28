6f1db37455768d0ac4ab9dc148b9106443babe30
Koukyosyumei <koukyosyumei@hotmail.com>
2021-08-28 17:18:04 +0900

    flip the sign of the gradients

--- src/core/utils.py
+++ src/core/utils.py
@@ -18,7 +18,7 @@ def compute_grad_update(old_model, new_model, device=None):
     if device:
         old_model, new_model = old_model.to(device), new_model.to(device)
     return [
-        new_param.data - old_param.data
+        old_param.data - new_param.data
         for old_param, new_param in zip(old_model.parameters(), new_model.parameters())
     ]
 
