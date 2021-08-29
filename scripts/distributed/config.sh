d1f357ec38a00bf302993fb6cb96fed5dab88c2f
Koukyosyumei <koukyosyumei@hotmail.com>
2021-08-29 16:00:24 +0900

    move most variables to config

---
+++ scripts/distributed/config.sh
@@ -0,0 +1,58 @@
+######## General settings ########
+# cluster setting
+node_type="grid_short.q"
+npernode=1
+gpu_mapping_yaml="gpu_mapping.yaml"
+
+# clients setting
+client_num=10
+client_num_per_round=5
+worker_num_pernode=1
+
+# py file to be executed
+py_file="./distributed_main.py"
+
+# model and optimier setting
+model="resnet56"
+comm_round=20
+epochs=1
+client_optimizer="adam"
+lr=0.001
+clip_grad=1
+max_norm=5.0
+
+# dataset setting
+dataset="cifar10"
+data_dir="/work/hideaki-t/dev/FedML/data/cifar10"
+partition_method="hetero"
+partition_alpha=0.3
+batch_size=64
+
+# other settings
+ci=0
+submit_script=1
+
+######## Method settings ########
+
+## AutoEncoder (AE) settings
+autoencoder_lr=0.01
+autoencoder_epochs=1
+
+## RFFL settings
+warm_up=10
+alpha=0.95
+sparcity=1
+remove=1
+
+## Quality Inferece (QI) settings
+
+######## Adversary settings ########
+adversary_num=2
+adversary_type="freerider"
+
+## Free-Rider settings
+free_rider_strategy="advanced-delta"
+
+## Inflator settings
+water_powered_magnification=1.0
+num_of_augmentation=0
