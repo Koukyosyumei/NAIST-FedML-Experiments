524fa80cb3e4d66541297a94117413a312c3c1c9
Koukyosyumei <koukyosyumei@hotmail.com>
2021-09-10 19:01:49 +0900

    change the id process

--- src/distributed/fedavg/fedavg_gradient_trainer.py
+++ src/distributed/fedavg/fedavg_gradient_trainer.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+import torch
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.getcwd(), "../../")))
 from core.utils import transform_grad_to_list
 
@@ -9,8 +11,16 @@ from fedml_api.distributed.fedavg.FedAVGTrainer import FedAVGTrainer
 
 
 class FedAVGGradTrainer(FedAVGTrainer):
+    def update_dataset(self, client_index):
+        self.client_index = client_index
+        self.trainer.set_id(client_index)
+        self.train_local = self.train_data_local_dict[client_index]
+        self.local_sample_number = self.train_data_local_num_dict[client_index]
+        self.test_local = self.test_data_local_dict[client_index]
+
     def train(self, round_idx=None):
         self.args.round_idx = round_idx
+
         grads = self.trainer.train(self.train_local, self.device, self.args)
 
         # transform Tensor to list
