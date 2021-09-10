115522458e492065d297203c984086bfba6e179a
Koukyosyumei <koukyosyumei@hotmail.com>
2021-09-10 19:05:16 +0900

    make SecureFedAVGServerManager the child class of ServerManager

--- src/core/distributed_secure_server_manager.py
+++ src/core/distributed_secure_server_manager.py
@@ -12,13 +12,37 @@ from fedml_api.distributed.fedavg.utils import (
     transform_tensor_to_list,
 )
 from fedml_core.distributed.communication.message import Message
+from fedml_core.distributed.server.server_manager import ServerManager
 
 
-class SecureFedAVGServerManager(FedAVGServerManager):
+class SecureFedAVGServerManager(ServerManager):
+    def __init__(
+        self,
+        args,
+        aggregator,
+        comm=None,
+        rank=0,
+        size=0,
+        backend="MPI",
+        is_preprocessed=False,
+        preprocessed_client_lists=None,
+    ):
+        super().__init__(args, comm, rank, size, backend)
+        self.args = args
+        self.aggregator = aggregator
+        self.round_num = args.comm_round
+        self.round_idx = 0
+        self.is_preprocessed = is_preprocessed
+        self.preprocessed_client_lists = preprocessed_client_lists
+        self.client_indexes = []
+
+    def run(self):
+        super().run()
+
     def send_init_msg(self):
         self.sender_id_to_client_index = {}
         # sampling clients
-        client_indexes = self.aggregator.client_sampling(
+        self.client_indexes = self.aggregator.client_sampling(
             self.round_idx,
             self.args.client_num_in_total,
             self.args.client_num_per_round,
@@ -26,11 +50,21 @@ class SecureFedAVGServerManager(FedAVGServerManager):
         global_model_params = self.aggregator.get_global_model_params()
         if self.args.is_mobile == 1:
             global_model_params = transform_tensor_to_list(global_model_params)
+
         for process_id in range(1, self.size):
-            self.send_message_init_config(
-                process_id, global_model_params, client_indexes[process_id - 1]
+            logging.info(
+                f"send init message to client_index = {process_id - 1} (process_id = {process_id})"
             )
-            self.sender_id_to_client_index[process_id] = client_indexes[process_id - 1]
+            self.send_message_init_config(
+                process_id, global_model_params, process_id - 1
+            )
+            self.sender_id_to_client_index[process_id] = process_id - 1
+
+    def register_message_receive_handlers(self):
+        self.register_message_receive_handler(
+            MyMessage.MSG_TYPE_C2S_SEND_MODEL_TO_SERVER,
+            self.handle_message_receive_model_from_client,
+        )
 
     def handle_message_receive_model_from_client(self, msg_params):
         sender_id = msg_params.get(MyMessage.MSG_ARG_KEY_SENDER)
@@ -46,6 +80,10 @@ class SecureFedAVGServerManager(FedAVGServerManager):
             if self.args.method == "RFFL":
                 reward_gradients = self.aggregator.aggregate(
                     self.sender_id_to_client_index
+                )
+            elif self.args.method == "QI":
+                global_model_params = self.aggregator.aggregate(
+                    self.sender_id_to_client_index, self.client_indexes
                 )
             else:
                 global_model_params = self.aggregator.aggregate(
@@ -66,43 +104,67 @@ class SecureFedAVGServerManager(FedAVGServerManager):
             if self.is_preprocessed:
                 if self.preprocessed_client_lists is None:
                     # sampling has already been done in data preprocessor
-                    client_indexes = [self.round_idx] * self.args.client_num_per_round
+                    self.client_indexes = [
+                        self.round_idx
+                    ] * self.args.client_num_per_round
                 else:
-                    client_indexes = self.preprocessed_client_lists[self.round_idx]
+                    self.client_indexes = self.preprocessed_client_lists[self.round_idx]
             else:
                 # sampling clients
-                client_indexes = self.aggregator.client_sampling(
+                self.client_indexes = self.aggregator.client_sampling(
                     self.round_idx,
                     self.args.client_num_in_total,
                     self.args.client_num_per_round,
                 )
 
-            # update the loolup table
-            for process_id in range(1, self.size):
-                self.sender_id_to_client_index[process_id] = client_indexes[
-                    process_id - 1
-                ]
-
-            print("indexes of clients: " + str(client_indexes))
+            print("indexes of clients: " + str(self.client_indexes))
             print("size = %d" % self.size)
 
             if self.args.method == "RFFL":
-                for receiver_id in range(1, self.size):
-                    params = reward_gradients[client_indexes[receiver_id - 1]]
+                for cid in self.client_indexes:
+                    params = reward_gradients[cid]
                     if self.args.is_mobile == 1:
                         params = transform_grad_to_list(params)
-                    self.send_message_sync_model_to_client(
-                        receiver_id,
-                        params,
-                        client_indexes[receiver_id - 1],
+
+                for process_id in range(1, self.size):
+                    logging.info(
+                        f"send updated parameters to client_index = {process_id - 1} (process_id = {process_id})"
                     )
+                    self.send_message_sync_model_to_client(
+                        process_id, params, process_id - 1
+                    )
+                    self.sender_id_to_client_index[process_id] = process_id - 1
             else:
                 if self.args.is_mobile == 1:
                     global_model_params = transform_tensor_to_list(global_model_params)
 
-                for receiver_id in range(1, self.size):
-                    self.send_message_sync_model_to_client(
-                        receiver_id,
-                        global_model_params,
-                        client_indexes[receiver_id - 1],
+                for process_id in range(1, self.size):
+                    logging.info(
+                        f"send updated parameters to client_index = {process_id - 1} (process_id = {process_id})"
                     )
+                    self.send_message_sync_model_to_client(
+                        process_id, global_model_params, process_id - 1
+                    )
+                    self.sender_id_to_client_index[process_id] = process_id - 1
+
+    def send_message_init_config(self, receive_id, global_model_params, client_index):
+        logging.info("Initial Configurations sent to client {0}".format(client_index))
+        message = Message(
+            MyMessage.MSG_TYPE_S2C_INIT_CONFIG, self.get_sender_id(), receive_id
+        )
+        message.add_params(MyMessage.MSG_ARG_KEY_MODEL_PARAMS, global_model_params)
+        message.add_params(MyMessage.MSG_ARG_KEY_CLIENT_INDEX, str(client_index))
+        self.send_message(message)
+
+    def send_message_sync_model_to_client(
+        self, receive_id, global_model_params, client_index
+    ):
+        logging.info("send_message_sync_model_to_client. receive_id = %d" % receive_id)
+        message = Message(
+            MyMessage.MSG_TYPE_S2C_SYNC_MODEL_TO_CLIENT,
+            self.get_sender_id(),
+            receive_id,
+        )
+        message.add_params(MyMessage.MSG_ARG_KEY_MODEL_PARAMS, global_model_params)
+        message.add_params(MyMessage.MSG_ARG_KEY_CLIENT_INDEX, str(client_index))
+        self.send_message(message)
